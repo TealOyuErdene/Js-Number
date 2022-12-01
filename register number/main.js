bd2f676eb73eadf1d9b1c60b17211305d7e582a1
TealOyuErdene <118173533+TealOyuErdene@users.noreply.github.com>
2022-12-02 03:03:33 +0800

    Add files via upload

---
+++ register number/main.js
@@ -0,0 +1,10 @@
+let register =prompt('Registeriin dugaaraa oruulna uu')
+
+console.log(register.length);
+console.log(register.substr(0,2));
+console.log(+register.substr(2,8));
+
+if(register.length = 10){
+    console.log('Thanks')
+    
+}
